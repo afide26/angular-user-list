35a5ef71297f10eb1e57d5f65845fd253bb71bac
Alan Fidelino <afidedev26@gmail.com>
2019-06-17 19:51:47 +1000

    Initialize application, add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import bootstrap from "bootstrap";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { TableComponent } from './table/table.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TableComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
